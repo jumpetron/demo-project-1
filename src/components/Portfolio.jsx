6e41e1e41cd67fe31554d902ceb6c379fd727540
Ahmed Foysal <ahmedfoysal01797@gmail.com>
2021-11-22 19:55:37 +0600

    service and portfolio section design

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,9 +1,78 @@
 import React from 'react'
+import styled from 'styled-components'
+import portfolio1 from '../assets/portfolio.jpg'
 
 export default function Portfolio() {
     return (
-        <div>
-            
-        </div>
+        <Section id="portfolio">
+            <div className="background">
+                <img src={portfolio1} alt="" />
+            </div>
+            <div className="content">
+                <h1>Buy Premium Pan Free Hand Toast</h1>
+                <h2>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod, sit.</h2>
+                <button>Buy Now</button>
+            </div>
+        </Section>
     )
 }
+
+const Section = styled.section`
+    height: 80vh;
+    position: relative;
+    border-radius: 5rem;
+    &:hover{
+        .background{
+            img{
+                transform: scale(1.2);
+            }
+        }
+    }
+
+    .background{
+        height: 100%;
+        max-width: 100%;
+        overflow: hidden;
+        border-radius: 1rem;
+        img{
+            object-fit: cover;
+            width: 100%;
+            height: 100%;
+            filter: brightness(60%);
+            border-radius: 1rem;
+            transition: 0.8s ease-in-out;
+        }
+    }
+
+    .content{
+        position: absolute;
+        top: 25%;
+        left: 10%;
+        color: #fff;
+        display: flex;
+        flex-direction: column;
+        gap: 1rem;
+        justify-content: center;
+        align-items: flex-start;
+        h1{
+            font-size: 3rem;
+            width: 60%;
+        }
+        h2{
+            width: 60%;
+        }
+        button{
+            border: none;
+            padding: 1rem 4rem;
+            border-radius: 4rem;
+            font-size: 1.4rem;
+            color: #fff;
+            background: linear-gradient(to right, #fc4958, #e85d05);
+            transition: 0.5s ease-in-out;
+            text-transform: uppercase;
+            &:hover{
+                background: linear-gradient(to left, #fc4958, #e85d05);
+            }
+        }
+    }
+`;
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,9 +1,85 @@
 import React from 'react'
+import styled from 'styled-components'
+import Services1 from '../assets/Services1.png'
+import Services2 from '../assets/Services2.png'
+import Services3 from '../assets/Services3.png'
+import {TitleStyles} from './ReusuableStyles'
 
 export default function Services() {
     return (
-        <div>
-            
-        </div>
+        <Section id="services">
+            <div className="title">
+                <h1 className="yellow">What we do?</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui nisi praesentium deserunt, numquam ipsum corrupti obcaecati officiis aliquid ab eligendi?</p>
+            </div>
+            <div className="services">
+                <div className="service">
+                    <img src={Services2} alt="" />
+                    <p>He Printing and Typesetting the industry <span>Lorem ipsum</span> dolor sit amet.</p>
+                    <button>Read More</button>
+                </div>
+
+                <div className="service yellow">
+                    <img src={Services1} alt="" />
+                    <p>He Printing and Typesetting the industry <span>Lorem ipsum</span> dolor sit amet.</p>
+                    <button>Read More</button>
+                </div>
+
+                <div className="service">
+                    <img src={Services3} alt="" />
+                    <p>He Printing and Typesetting the industry <span>Lorem ipsum</span> dolor sit amet.</p>
+                    <button>Read More</button>
+                </div>
+            </div>
+        </Section>
     )
 }
+
+const Section = styled.section`
+    margin: 2rem 4rem;
+    ${TitleStyles}
+    .services{
+        display: grid;
+        grid-template-columns: repeat(3, 1fr);
+        gap: 10vw;
+        margin-top: 4rem;
+        .service{
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            gap: 1.5vw;
+            padding: 0 3vw;
+            img{
+                height: 2.8rem;
+            }
+            p{
+                text-align: center;
+                line-height: 2rem;
+                font-size: 1.1rem;
+            }
+            button{
+                padding: 0.6rem 3rem;
+                letter-spacing: 0.1rem;
+                border-radius: 2rem;
+                font-size: 1.1rem;
+                border: none;
+                color: #fff;
+                cursor: pointer;
+                background-color: #fc4958;
+                transition: 0.3s ease-in-out;
+                &:hover{
+                    background-color: #f9c74f;
+                }
+            }
+        }
+
+        .yellow{
+            button{
+                background-color: #f9c74f;
+                &:hover{
+                    background-color: #fc4958
+                }
+            }
+        }
+    }
+`;
